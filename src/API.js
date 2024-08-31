b9cbe6c48fb18d1d2584dc1a350bad9174366f27
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-08-31 16:37:58 +0530

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://hr-candidate-server.onrender.com";
 
 async function login(loginData){
     const result = await axios.post(`${baseUrl}/login`,loginData);
